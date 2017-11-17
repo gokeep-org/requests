b4c8ed2db120288f0e42672a6c7fd16df15d0734
xuning <xuning@deepdata.com.cn>
2017-11-17 14:05:19 +0800

    修改timeout时间为30秒，添加请求示例

--- src/main/java/cn/networklab/requests/core/DefaultConfig.java
+++ src/main/java/cn/networklab/requests/core/DefaultConfig.java
@@ -6,8 +6,8 @@ import org.apache.http.HttpVersion;
 
 public class DefaultConfig {
     public static final HttpVersion HTTP_VERSION = HttpVersion.HTTP_1_1;
-    public static final int SOCKET_TIMOUT = 6000;
-    public static final int CONNECTION_TIMOUT = 6000;
+    public static final int SOCKET_TIMOUT = 30000;
+    public static final int CONNECTION_TIMOUT = 30000;
     public static final String REQUEST_CHARSET = "UTF-8";
     public static final String CONTENT_TYPE = "application/json";
     public static final String LOGGER_FORMATE = "url:[%s], params:[%s], postform:[%s], body:[%s]";
